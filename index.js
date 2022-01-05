7381976da172e15d65f8abe532c9c5a065de52e2
faizazafar <faiza99826@gmail.com>
2022-01-05 16:00:32 +0500

    commitnew

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import New from './New';
 import {name as appName} from './app.json';
+import Login from './src/components/Login';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,110 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+// import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import {useState} from 'react';
 
 export default function Login() {
-    return (
-        <View>
-            <Text>Login</Text>
-        </View>
-    )
+  const [email, setEmail] = useState('');
+  const [pass, setPassword] = useState('');
+  const [showText, setShowText] = useState(true);
+
+  const handleSubmitPress = () => {
+    if (!email) {
+      alert('Please enter user');
+      return;
+    }
+    if (!pass) {
+      alert('Please fill Password');
+      return;
+    } else {
+      console.info(email, pass);
+    }
+  };
+
+  console.log(email);
+  console.log(pass);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text1}>SIGN IN </Text>
+
+      <View style={styles.Loginform}>
+        <Feather style={{flex: 0.1}} name={'user'} size={20} color="#62757f" />
+        <TextInput
+          style={{flex: 0.8}}
+          onChangeText={text => setEmail(text)}
+          placeholder="Username"></TextInput>
+      </View>
+      <View style={styles.Loginform}>
+        <Feather style={{flex: 0.1}} name={'lock'} size={20} color="#62757f" />
+
+        <TextInput
+          style={{flex: 0.7}}
+          placeholder={'Password'}
+          onChangeText={text => setPassword(text)}
+          secureTextEntry={showText}></TextInput>
+
+        <Feather
+          style={{flex: 0.1}}
+          size={20}
+          color="#62757f"
+          name={showText ? 'eye-off' : 'eye'}
+          onPress={() => setShowText(!showText)}
+        />
+      </View>
+      <View style={{flexDirection: 'row', paddingTop: 10, paddingBottom: 40}}>
+        <Text style={{color: '#707070', fontWeight: '500'}}>
+          Don't Have an Account?
+        </Text>
+        <TouchableOpacity>
+          <Text style={{color: '#7e57c2', fontWeight: 'bold'}}>
+            Register Here
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        style={{
+          padding: 20,
+          backgroundColor: 'black',
+          alignItems: 'center',
+          width: 200,
+          borderRadius: 30,
+        }}
+        onPress={handleSubmitPress}>
+        <Text style={{color: 'white', fontWeight: '600'}}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 40,
+  },
+
+  text1: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: 'black',
+    paddingBottom: 40,
+  },
+  Loginform: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#cfd8dc',
+    marginVertical: 5,
+  },
+});
