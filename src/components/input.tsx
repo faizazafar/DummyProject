87537f0750619fbce0635735cb27bf1c2cf5b5ed
faizazafar <faiza99826@gmail.com>
2022-01-14 18:54:02 +0500

    newwww

---
+++ src/components/input.tsx
@@ -0,0 +1,21 @@
+import { View, Text, TextInput } from 'react-native'
+import React , { FC} from 'react'
+
+interface Props {
+
+
+
+}
+
+
+const input : FC <Props>=() => {
+    return (
+        <View>
+            <View>
+             <TextInput placeholderTextColor={}></TextInput>
+            </View>
+        </View>
+    )
+}
+
+export default input
